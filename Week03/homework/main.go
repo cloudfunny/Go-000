74bce114bb8a6940168123ab62c3581a9c59de7c
mohuishou <1@lailin.xyz>
2020-12-11 16:06:44 +0800

    fix(homework): 修复服务意外退出但是主进程不会退出的问题

--- Week03/homework/main.go
+++ Week03/homework/main.go
@@ -7,54 +7,64 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"net/http"
 	_ "net/http/pprof"
 
+	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
 	g, ctx := errgroup.WithContext(context.Background())
-	ctx, cancel := context.WithCancel(ctx)
 
-	g.Go(func() error {
-		return server(ctx, http.DefaultServeMux, ":8081")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
 	})
 
-	g.Go(func() error {
-		mux := http.NewServeMux()
-		mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("pong"))
-		})
-		return server(ctx, mux, ":8080")
+	// 模拟单个服务错误退出
+	serverOut := make(chan struct{})
+	mux.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
+		serverOut <- struct{}{}
 	})
 
-	quit := make(chan os.Signal, 0)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-
-	<-quit
-	fmt.Println("")
-	log.Println("shutting down server...")
-	cancel()
-
-	fmt.Printf("errgroup exiting: %+v\n", g.Wait())
-}
-
-// 启动一个服务
-func server(ctx context.Context, handler http.Handler, addr string) error {
-	s := http.Server{
-		Handler: handler,
-		Addr:    addr,
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
 	}
 
-	// 这个 goroutine 我们可以控制退出，因为只要 stop 这个 channel close 或者是写入数据，这里就会退出
-	// 同时因为调用了 s.Shutdown 调用之后，http 这个函数启动的 http server 也会优雅退出
-	go func() {
-		<-ctx.Done()
-		log.Printf("server will exiting, addr: %s", addr)
-		s.Shutdown(context.Background())
-	}()
+	g.Go(func() error {
+		return server.ListenAndServe()
+	})
 
-	return s.ListenAndServe()
+	g.Go(func() error {
+		select {
+		case <-ctx.Done():
+			log.Println("errgroup exit...")
+		case <-serverOut:
+			log.Println("server will out...")
+		}
+
+		timeoutCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+
+		log.Println("shutting down server...")
+		return server.Shutdown(timeoutCtx)
+	})
+
+	g.Go(func() error {
+		quit := make(chan os.Signal, 0)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case sig := <-quit:
+			return errors.Errorf("get os signal: %v", sig)
+		}
+	})
+
+	fmt.Printf("errgroup exiting: %+v\n", g.Wait())
 }
